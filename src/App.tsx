d438f042640251dc776fa12c9f590f102416608e
Luan Oliveira <luanoliveira17@hotmail.com>
2023-03-16 18:33:24 -0300

    structure: add global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
-import { defaultTheme } from './styles/themes/default'
+import { ThemeProvider } from 'styled-components';
+import { Button } from './components/Button';
+
+import { GlobalStyle } from './styles/global';
+import { defaultTheme } from './styles/themes/default';
 
 export function App() {
   return (
@@ -10,6 +12,8 @@ export function App() {
       <Button variant="danger"/>
       <Button variant="success"/>
       <Button/>
+
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
